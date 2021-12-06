774cfa492949ce5cd5b34a580f30bed255a90187
Parky <parkyrosh@outlook.com>
2021-12-05 21:53:46 -0800

    first commit

---
+++ sitarabank.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class sitarabank
+{
+    public static void main (String[]args)
+    {
+        //variables start here
+
+        //variables end here
+
+        //user interface start here
+         Scanner sc = new Scanner(System.in);
+         System.out.println("");
+         System.out.println("KINDLY ENTER YOUR FIRST NAME");
+         String namone = sc.nextLine();
+         System.out.println("KINDLY ENTER YOUR SURNAME");
+         String namtwo = sc.nextLine();
+         System.out.println("");
+         System.out.println("HELLO-"+namone.toUpperCase());
+         System.out.println("WELCOME TO SITARA BANKING SERVER");
+         System.out.println("");
+         System.out.println("WE ARE UNDER DEVLOPMENT YOU CAN FACE SOME ERRORS");
+         System.out.println("");
+         System.out.println("AS PER BANKING NORMS WE NEED TO STORE YOUR NAME AND IP ADDRESS FOR SECURITY PURPOSE");
+         System.out.println("");
+
+         //user interface end here
+
+    }
+}
